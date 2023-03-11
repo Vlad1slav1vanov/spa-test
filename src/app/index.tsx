007bb84877bd44bbef9324e71b99d990d639a0f1
Vladislav <typlomi@gmail.com>
2023-03-11 15:56:03 +0300

    init app

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/index.scss'
+
+const App: React.FC = () => {
+  return (
+    <div className='app-wrapper'>
+      HELLO
+    </div>
+  )
+}
+
+export default App
